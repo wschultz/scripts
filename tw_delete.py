796c0841b26236664f44bd2bbd16c889171eaf19
wschultz <wschultz@dropbox.com>
2023-04-14 22:59:06 -0500

    Adding new tweet delete script

---
+++ tw_delete.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+ 
+import os
+import twitter
+from time import time
+
+# Create an Api instance.
+api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
+                  consumer_secret=os.environ.get('CONSUMER_SECRET'),
+                  access_token_key=os.environ.get('ACCESS_TOKEN'),
+                  access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'))
+
+# Change these variables here
+days_ago = 4                 # How many days do we keep?
+screen_name = "vileTexan"    # What's your twitter handle?
+
+# What are the Tweet IDs you don't want to delete?
+safe_ids = [
+  1638218044877971470,
+]
+
+cutoff = int(time()) - (days_ago * 24 * 60 * 60)
+max_id = 0
+tweets = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=True) 
+
+while len(tweets) > 1:
+  for tweet in tweets:
+    if tweet.created_at_in_seconds < cutoff and tweet.id not in safe_ids:
+      print(tweet.id, tweet.text)
+      api.DestroyStatus(status_id=tweet.id)
+
+    max_id = tweet.id
+
+  tweets = api.GetUserTimeline(screen_name="vileTexan", count=200, include_rts=True, max_id=max_id)
+
+  if len(tweets) == 1:
+    api.DestroyStatus(status_id=tweet.id)
+    break
+
--- tweet_delete.py
+++ tweet_delete.py
@@ -5,9 +5,9 @@ import tweepy
 from datetime import datetime, timedelta
  
 # options
-test_mode = False
+test_mode = True
 verbose = True
-delete_tweets = True
+delete_tweets = False
 delete_favs = True
 days_to_keep = 2
  
@@ -37,7 +37,7 @@ cutoff_date = datetime.utcnow() - timedelta(days=days_to_keep)
 # delete old tweets
 if delete_tweets:
     # get all timeline tweets
-    print "Retrieving timeline tweets"
+    print("Retrieving timeline tweets")
     timeline = tweepy.Cursor(api.user_timeline).items()
     deletion_count = 0
     ignored_count = 0
@@ -47,7 +47,7 @@ if delete_tweets:
         if tweet.id not in tweets_to_save and tweet.created_at < cutoff_date:
             if verbose:
                 #print "Deleting %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text)
-                print "Deleting %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text.encode("utf-8"))
+                print("Deleting %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text.encode("utf-8")))
             if not test_mode:
                 api.destroy_status(tweet.id)
              
@@ -55,15 +55,15 @@ if delete_tweets:
         else:
             ignored_count += 1
  
-    print "Deleted %d tweets, ignored %d" % (deletion_count, ignored_count)
+    print("Deleted %d tweets, ignored %d" % (deletion_count, ignored_count))
 else:
-    print "Not deleting tweets"
+    print("Not deleting tweets")
      
 # unfavor old favorites
 if delete_favs:
     # get all favorites
-    print "Retrieving favorite tweets"
-    favorites = tweepy.Cursor(api.favorites).items()
+    print("Retrieving favorite tweets")
+    favorites = tweepy.Cursor(api.get_favorites()).items()
     unfav_count = 0
     kept_count = 0
  
@@ -72,7 +72,7 @@ if delete_favs:
         if tweet.id not in favs_to_save and tweet.created_at < cutoff_date:
             if verbose:
                 #print "Unfavoring %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text)
-                print "Unfavoring %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text.encode("utf-8"))
+                print("Unfavoring %d: [%s] %s" % (tweet.id, tweet.created_at, tweet.text.encode("utf-8")))
             if not test_mode:
                 api.destroy_favorite(tweet.id)
              
@@ -80,7 +80,7 @@ if delete_favs:
         else:
             kept_count += 1
  
-    print "Unfavored %d tweets, ignored %d" % (unfav_count, kept_count)
+    print("Unfavored %d tweets, ignored %d" % (unfav_count, kept_count))
 else:
-    print "Not unfavoring tweets"
+    print("Not unfavoring tweets")
 
