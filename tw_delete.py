0314a20ba1069893e0d5ef98a969a4003ab95a28
wschultz <wschultz@dropbox.com>
2023-04-22 14:19:51 -0500

    Adding in some more logic, and a def to blast favorites also... this seems somewhat broken by the API though

--- tw_delete.py
+++ tw_delete.py
@@ -11,8 +11,10 @@ api = twitter.Api(consumer_key=os.environ.get('CONSUMER_KEY'),
                   access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET'))
 
 # Change these variables here
-days_ago = 3                 # How many days do we keep?
-screen_name = "vileTexan"    # What's your twitter handle?
+days_ago         = 3           # How many days do we keep?
+screen_name      = "vileTexan" # What's your twitter handle?
+delete_tweets    = True        # True/False... If True, delete Tweets
+delete_favorites = True        # True/False... If True, delete Favorites
 
 # What are the Tweet IDs you don't want to delete?
 safe_ids = [
@@ -21,19 +23,57 @@ safe_ids = [
 
 cutoff = int(time()) - (days_ago * 24 * 60 * 60)
 max_id = 0
-tweets = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=True) 
 
-while len(tweets) > 1:
-  for tweet in tweets:
-    if tweet.created_at_in_seconds < cutoff and tweet.id not in safe_ids:
-      print(tweet.id, tweet.text)
-      api.DestroyStatus(status_id=tweet.id)
+def DeleteTweets(cutoff):
+  tweets = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=True) 
 
-  max_id = tweet.id
+  try:
+    while len(tweets) > 1:
+      for tweet in tweets:
+        if tweet.created_at_in_seconds < cutoff and tweet.id not in safe_ids:
+          print(tweet.id, tweet.text)
+          api.DestroyStatus(status_id=tweet.id)
 
-  tweets = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=True, max_id=max_id)
+      max_id = tweet.id
 
-  if len(tweets) == 1:
-    api.DestroyStatus(status_id=tweet.id)
-    break
+      tweets = api.GetUserTimeline(screen_name=screen_name, count=200, include_rts=True, max_id=max_id)
+
+      if len(tweets) == 1:
+        api.DestroyStatus(status_id=tweet.id)
+        break
+
+  except Exception as err:
+    print(err.message)
+    for i in err.message:
+      print(i)
+
+
+def DeleteFavorites(cutoff):
+  favs = api.GetFavorites(screen_name=screen_name, count=200)
+
+  try:
+    while len(favs) > 1:
+      for fav in favs:
+        if fav.created_at_in_seconds < cutoff:
+          print(fav.id, fav.text)
+          api.DestroyFavorite(status_id=fav.id)
+
+      max_id = fav.id
+
+      favs = api.GetFavorites(screen_name=screen_name, count=200, max_id=max_id)
+
+      if len(favs) == 1:
+        api.DestroyFavorite(status_id=fav.id)
+        break
+
+  except Exception as err:
+    print(err.message)
+    for i in err.message:
+      print(i)
+
+if __name__ == "__main__":
+  if delete_tweets == True:
+    DeleteTweets(cutoff)
+  if delete_favorites == True:
+    DeleteFavorites(cutoff)
 
